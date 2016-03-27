cf55b403b1c63f4752c13d9bf92f12b944a683ee
EC2 Default User <ec2-user@ip-172-31-3-247.us-west-1.compute.internal>
2016-03-27 05:42:39 +0000

    Adding a driver to call genetics world.

---
+++ include/gw_driver.h
@@ -0,0 +1,47 @@
+// gw_driver.h
+
+#ifndef GW_DRIVER
+#define GW_DRIVER
+
+namespace GeneticsWorld
+{
+    template <class Creature, template <class> World>
+    class Driver {
+    public:
+        Driver();
+        ~Driver();
+        void setup_world();
+        void time_step();
+    private:
+        World _world;
+    };
+};
+
+template <class Creature, template <class> World>
+GeneticsWorld::Driver::Driver()
+{
+}
+
+template <class Creature, template <class> World>
+GeneticsWorld::Driver::~Driver()
+{
+}
+
+template <class Creature, template <class> World>
+void GeneticsWorld::Driver::setup_world()
+{
+    for (size_t c = 0; c < 100; ++c)
+    {
+        _world.add_creature();
+    }
+}
+
+template <class Creture, template <class> World>
+void GeneticsWorld::Driver::time_step()
+{
+    world.time_step();
+    world.clear_dead_creatures();
+    world.spawn_born_creatures();
+}
+
+#endif // GW_DRIVER
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 #include "genetics_world.h"
+#include "gw_driver.h"
+#include "gw_world.h"
+#include "gw_creature.h"
 
 int main(int argc, char** argv)
 {
-    std::cout << "Genetics world v" << GENETICS_WORLD_VERSION_MAJOR << "." << GENETICS_WORLD_VERSION_MINOR << std::endl;
+    std::cout << "Genetics world v" <<
+                 GENETICS_WORLD_VERSION_MAJOR << "." <<
+                 GENETICS_WORLD_VERSION_MINOR << std::endl;
+
+    GeneticsWorld::Driver<GeneticsWorld::BasicCreature,
+                          GeneticsWorld::World<GeneticsWorld::BasicCreature> > driver();
+    driver.setup();
+    for (size_t t = 0; t < 10000; ++t)
+    {
+        driver.time_step();
+    }
     return 0;
 }
